dbb927bf397f825da4e80f0cd2fca021bdeb1e08
aryclemente <hello.aryc@gmail.com>
2023-05-15 00:08:29 -0400

    juego de pacman

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -4,10 +4,14 @@ function activar2() {
   if (token) {
     let div = document.getElementById("nuevoboton");
 
-    div.innerHTML = `  <h2 class="text-start mb-4">Bienvenido</h2>
+    div.innerHTML = `              
+            <h2 class="text-start mb-4">Bienvenido</h2>
             <p class="lead">
               Has iniciado sesión como <span id="username"> ${token}</span>
-            </p>`;
+            </p>
+                  `;
+                  window.location.href = "../pages/pacman.html";         
+            
   } else {
     window.location.href = "../index.html";
   }
