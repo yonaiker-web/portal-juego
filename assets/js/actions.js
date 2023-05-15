747dc94237e8531b9f0daefb3045741a807c7ef7
yonaiker <yonaiker.ocando@unidigital.global>
2023-05-15 10:05:29 -0400

    .

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -2,16 +2,7 @@
 function activar2() {
   const token = localStorage.getItem("token");
   if (token) {
-    let div = document.getElementById("nuevoboton");
-
-    div.innerHTML = `              
-            <h2 class="text-start mb-4">Bienvenido</h2>
-            <p class="lead">
-              Has iniciado sesión como <span id="username"> ${token}</span>
-            </p>
-                  `;
-                  window.location.href = "../pages/pacman.html";         
-            
+    window.location.href = "../pages/pacman.html";
   } else {
     window.location.href = "../index.html";
   }
